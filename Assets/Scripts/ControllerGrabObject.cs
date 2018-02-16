528a7843290bf00db611bef3c655c0deb0b24626
mtsandmeyer <32112441+mtsandmeyer@users.noreply.github.com>
2018-02-16 10:31:52 -0500

    Initial commit

---
+++ Assets/Scripts/ControllerGrabObject.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerGrabObject : MonoBehaviour {
+
+    // The tracked object (controller) to get input from
+    private SteamVR_TrackedObject trackedObj;
+    // Stores the GameObject that the trigger is currently colliding with, so you have the ability to grab the object.
+    private GameObject collidingObject;
+    // Serves as a reference to the GameObject that the player is currently grabbing.
+    private GameObject objectInHand;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    /// <summary>
+    /// This method accepts a collider as a parameter and uses its GameObject as the collidingObject for grabbing and releasing.
+    /// </summary>
+    /// <param name="col"></param>The collider accepted
+    private void SetCollidingObject(Collider col)
+    {
+        // Doesn’t make the GameObject a potential grab target if the player is already holding something or the object has no rigidbody.
+        if (collidingObject || !col.GetComponent<Rigidbody>())
+        {
+            return;
+        }
+        // Assigns the object as a potential grab target.
+        collidingObject = col.gameObject;
+    }
+
+    // When the trigger collider enters another, this sets up the other collider as a potential grab target.
+    public void OnTriggerEnter(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    // Ensures that the target is set when the player holds a controller over an object for a while. Without this, the collision may fail or become buggy.
+    public void OnTriggerStay(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    // When the collider exits an object, abandoning an ungrabbed target, this code removes its target by setting it to null.
+    public void OnTriggerExit(Collider other)
+    {
+        if (!collidingObject)
+        {
+            return;
+        }
+
+        collidingObject = null;
+    }
+
+    // Grabs an object
+    private void GrabObject()
+    {
+        // Move the GameObject inside the player’s hand and remove it from the collidingObject variable
+        objectInHand = collidingObject;
+        collidingObject = null;
+        // Add a new joint that connects the controller to the object using the AddFixedJoint() method below.
+        var joint = AddFixedJoint();
+        joint.connectedBody = objectInHand.GetComponent<Rigidbody>();
+    }
+
+    // Make a new fixed joint, add it to the controller, and then set it up so it doesn’t break easily. Finally, you return it. 
+    private FixedJoint AddFixedJoint()
+    {
+        FixedJoint fx = gameObject.AddComponent<FixedJoint>();
+        fx.breakForce = 20000;
+        fx.breakTorque = 20000;
+        return fx;
+    }
+
+    private void ReleaseObject()
+    {
+        // Make sure there’s a fixed joint attached to the controller.
+        if (GetComponent<FixedJoint>())
+        {
+            // Remove the connection to the object held by the joint and destroy the joint.
+            GetComponent<FixedJoint>().connectedBody = null;
+            Destroy(GetComponent<FixedJoint>());
+            // Add the speed and rotation of the controller when the player releases the object, so the result is a realistic arc.
+            objectInHand.GetComponent<Rigidbody>().velocity = Controller.velocity;
+            objectInHand.GetComponent<Rigidbody>().angularVelocity = Controller.angularVelocity;
+        }
+        // Remove the reference to the formerly attached object.
+        objectInHand = null;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Controller.GetHairTriggerDown())
+        {
+            if (collidingObject)
+            {
+                GrabObject();
+            }
+        }
+
+        if (Controller.GetHairTriggerUp())
+        {
+            if (objectInHand)
+            {
+                ReleaseObject();
+            }
+        }
+    }
+}
